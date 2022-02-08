cca63382c635ad22cadc59e45f0436a316ffa4dd
John <johnartuz@hotmail.com>
2022-02-07 22:27:28 -0500

    Implement user authenthication

--- app/models/user_spec.rb
+++
@@ -1,6 +0,0 @@
-class UserSpec < ActiveRecord::Base
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :password_digest, presence: true
-end
