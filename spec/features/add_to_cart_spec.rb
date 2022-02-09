4e6b221d9be8aaebbd04180c8370b075f2e4e0d3
John <johnartuz@hotmail.com>
2022-02-08 23:51:17 -0500

    Add screeshot for adding a product to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+    
+
+  scenario "They click on add button and cart updates" do
+    # ACT
+    visit root_path
+    # DEBUG / VERIFY
+    
+    expect(page).to have_content('My Cart (0)')
+    click_button 'Add'
+    expect(page).to have_content('My Cart (1)')
+    save_screenshot
+
+  end
+end
